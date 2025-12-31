0786ce33fe6298e3e88dfd5195f449b4e6abb92d
muhamadatif <mohamedabotreka40@gmail.com>
2025-12-31 13:33:06 +0200

    test-auto update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="bg-rose-500 w-full h-dvh">
+    <div className="bg-blue-500 w-full h-dvh">
       <UpdateNotifier />
       <div>
         <a href="https://electron-vite.github.io" target="_blank">
